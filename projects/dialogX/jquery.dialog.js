641c1b195d45a7c23f7b135df37994365a0f6dbd
Joy Kuang <q-cute@163.com>
2016-02-18 19:24:08 +0800

    新增 DialogX 插件

---
+++ projects/dialogX/jquery.dialog.js
@@ -0,0 +1,48 @@
+;(function($){
+    var D = {};
+    
+    var details = {
+        version: '1455787608588',
+        author: 'Joy Kuang',
+        copyright: '(c)Yueruan Studio 2016',
+        license: 'MIT',
+        repository: 'https://coding.net/joykuang/dialogx.git'
+    };
+    
+    var defaults = {
+
+    };
+    
+    // 对话框
+    D.dialog = function(){
+        
+    },
+    
+    // 加载框
+    D.loading = function(){
+        
+    },
+    
+    // 弹出框
+    D.alert = function(){
+        
+    },
+    
+    // 确认框
+    D.confirm = function(){
+        
+    },
+    
+    // 输入框
+    D.prompt = function(){
+        
+    },
+    
+    // 事件
+    D.event = function(){
+        
+    },
+    
+    
+    $.dialogX = D;
+})(jQuery||Zepto)
